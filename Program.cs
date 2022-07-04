f4930fb52743dbb6004269ba99d8f064b597e047
Tim Jones <timothy.jones@gmx.us>
2022-07-04 01:27:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+app.MapGet("/addNums/{x}/{y}", (int x, int y) =>
+{
+    
+    return x+y;
+})
+.WithName("addNumbers");
+
+
+
+
+
+
+
+
+
+app.Run();
+
